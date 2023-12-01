51dc8910ecd71240768398599a81040be27f6207
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2023-12-01 11:45:43 -0300

    Add mongodb and service settings as services

--- src/Play.Catalog.Service/Program.cs
+++ src/Play.Catalog.Service/Program.cs
@@ -1,13 +1,30 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+using Play.Catalog.Service.Repositories;
+using Play.Catalog.Service.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
 BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String)); // Converts the Guid stored on mongodb into a string or better readability
 BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String)); // Converts the CreatedTime on mongodb into a string for better readability
+
+var serviceSettings = builder.Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+
+builder.Services.AddSingleton(serviceProvider =>
+{
+    var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+    var mongoClient = new MongoClient(mongoDbSettings.connectionString);
+
+    return mongoClient.GetDatabase(serviceSettings.ServiceName);
+});
+
+builder.Services.AddSingleton<IItemsRepository, ItemsRepository>();
+
 builder.Services.AddControllers(options =>
 {
     options.SuppressAsyncSuffixInActionNames = false;
