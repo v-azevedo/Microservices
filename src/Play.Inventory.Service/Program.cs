52a598bd18b69524a0391e694a1b6dd8edc730c7
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2023-12-05 23:40:23 -0300

    Implement policies to handle timeout requests

--- src/Play.Inventory.Service/Program.cs
+++ src/Play.Inventory.Service/Program.cs
@@ -1,8 +1,11 @@
 using Play.Commonn.MongoDB;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
+using Polly;
+using Polly.Timeout;
 
 var builder = WebApplication.CreateBuilder(args);
+Random jitterer = new Random();
 
 HttpClientHandler clientHandler = new HttpClientHandler();
 
@@ -17,7 +20,10 @@ builder.Services.AddHttpClient<CatalogClient>(client =>
 .ConfigurePrimaryHttpMessageHandler(_ => new HttpClientHandler
 {
     ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; } // Self sign certificate not being trusted on localhost, quick fix, MUST CHANGE BEFORE HOSTING.
-});
+})
+.AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
++ TimeSpan.FromMilliseconds(jitterer.Next(0, 1000))))
+.AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
